28c47562a2173f9f6c679079992e25a68c744f74
Joaco <jnfernandez.ojeda@gmail.com>
2021-05-11 13:30:25 -0300

    Staff Permissions to CRUD Views

--- webplayground/pages/views.py
+++ webplayground/pages/views.py
@@ -1,9 +1,22 @@
-from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.contrib.admin.views.decorators import staff_member_required
+from django.shortcuts import render, get_object_or_404, get_list_or_404, redirect
 from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .forms import PageForm
 from .models import Page
 
+"""
+# DEPRECATED, just to show how to extends features with inheritance.
+# HOW IT WAS USED: It was inherited from a CRUD view to add staff permissions. 
+class StaffRequiredMixin:
+    # Requires that logged user must be a staff member.
+
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_staff:
+            return redirect(reverse_lazy('admin:login'))
+        return super(StaffRequiredMixin, self).dispatch(request, *args, **kwargs)
+"""
 
 """
 # OLD VIEW FUNCTION
@@ -29,12 +42,14 @@ class PageDetailView(DetailView):
     model = Page
 
 
+@method_decorator(staff_member_required, name='dispatch')
 class PageCreateView(CreateView):
     model = Page
     form_class = PageForm
     success_url = reverse_lazy('pages:pages')
 
 
+@method_decorator(staff_member_required, name='dispatch')
 class PageUpdateView(UpdateView):
     model = Page
     form_class = PageForm
@@ -44,6 +59,7 @@ class PageUpdateView(UpdateView):
         return reverse_lazy('pages:update', args=[self.object.id]) + '?ok'
 
 
+@method_decorator(staff_member_required, name='dispatch')
 class PageDeleteView(DeleteView):
     model = Page
     success_url = reverse_lazy('pages:pages')
