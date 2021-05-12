9727dbb49482c6a7d5c5a6d5942d6252e8738d0d
Joaco <jnfernandez.ojeda@gmail.com>
2021-05-12 18:37:42 -0300

    Signals and Change Password Templates

--- webplayground/registration/urls.py
+++ webplayground/registration/urls.py
@@ -4,6 +4,7 @@ from .views import UserSignUp, ProfileUpdate, EmailUpdate
 
 urlpatterns = [
     path('signup/', UserSignUp.as_view(), name='signup'),
+    # We overwrite get_object, that is why we do no need a pk as parameter for every update view.
     path('profile/', ProfileUpdate.as_view(), name='profile'),
     path('profile/email/', EmailUpdate.as_view(), name='profile_email')
 ]
